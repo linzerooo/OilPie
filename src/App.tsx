fab5c7bf0222df8b9b6032e214fc59e4621a2043
Liana <a>
2025-05-20 22:27:03 +0300

    Добавлена возможность открывать маршрут.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
-import { WellsTable } from './Components/Wells';
-import { Reports } from './Components/Reports';
+import { WellsTable } from "./Components/Wells";
+import { Reports } from "./Components/Reports";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Главная</Link>
+        <Link to="/reports" style={{ marginLeft: "10px" }}>
+          Отчеты
+        </Link>
+      </nav>
       <Routes>
-        <Route path="/reports" element={<Reports />} />
         <Route path="/" element={<WellsTable />} />
+        <Route path="/reports/:id" element={<Reports />} />
       </Routes>
     </Router>
   );
