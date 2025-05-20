fe4333b4512306310eddb987d09e4e68931e8cb4
Liana <a>
2025-05-20 20:28:53 +0300

    добавлена маршрутизация

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
-import { Wells } from './Wells';
-import { Reports } from './Reports';
+import { WellsTable } from './Components/Wells';
+import { Reports } from './Components/Reports';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/reports" element={<Reports />} />
-        <Route path="/" element={<Wells />} />
+        <Route path="/" element={<WellsTable />} />
       </Routes>
     </Router>
   );
