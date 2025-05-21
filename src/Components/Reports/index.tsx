b652dcbb0cf8863069d4c38f59935449723e13b5
Liana <a>
2025-05-21 16:32:54 +0300

    Добавлен функционал Экспорт в Excel

--- src/Components/Reports/index.tsx
+++ src/Components/Reports/index.tsx
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 import { useState } from "react";
 import { AddReportModal } from "./ReportModal";
 import type { RootState } from "../Helpers/store";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 export const Reports = () => {
   const { id } = useParams<{ id: string }>();
@@ -18,10 +20,24 @@ export const Reports = () => {
     { title: "Проблемы", dataIndex: "issues", key: "3" },
   ];
 
+  const exportToExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(reports)
+    const workbook = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Отчёты')
+   
+    const excelBuffer = XLSX.write(workbook, {bookType: 'xlsx', type: "array"})
+    const file = new Blob([excelBuffer], { type: 'application/octet-stream' })
+    saveAs(file, `Отчёт скважина №${id}.xlsx}`)
+
+  }
+
   return (
     <>
       <Button onClick={() => setModalOpen(true)} style={{ marginBottom: 16}}>
         Добавить отчет
+      </Button>
+      <Button onClick={exportToExcel}> 
+        Экспорт в Excel
       </Button>
       <Table columns={columns} dataSource={reports} rowKey={(r) => r.date + r.engineer} />
       <AddReportModal wellId={id || "0"} open={modalOpen} onClose={() => setModalOpen(false)} />
